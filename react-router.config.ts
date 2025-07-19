cc5be2964266d9a614e82d36b35a9e260545efd8
Omar <pro_omrlock@outlo0k.com>
2025-07-19 14:56:30 +0300

    enable prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from "@react-router/dev/config";
-import { copyFileSync } from "node:fs";
-import { join } from "node:path";
+import { copyFileSync, readdirSync, renameSync, rmdirSync } from "node:fs";
+import path, { join } from "node:path";
 
 export default {
   basename: "/portfolio-v1/",
   ssr: false,
+  prerender: true,
   buildEnd(args) {
     if (!args.viteConfig.isProduction) return;
-
     // When deploying to GitHub Pages, if you navigate from / to another
     // route and refresh the tab, it will show the default GH Pages 404
     // page. This happens because GH Pages is not configured to send all
@@ -17,6 +17,20 @@ export default {
     // GH Pages will serve our 404.html and everything will work as
     //expected.
     const buildPath = args.viteConfig.build.outDir;
-    copyFileSync(join(buildPath, "index.html"), join(buildPath, "404.html"));
+    copyFileSync(
+      join(buildPath, "portfolio-v1/index.html"),
+      join(buildPath, "portfolio-v1/404.html"),
+    );
+
+    // Due to enabling prerendering, the build output will be placed in build/client/portfolio-v1
+    // instead of build/client. We need to move the contents of that folder
+    // to the build/client folder so that the server can serve the files correctly.
+    const folderPath = join(buildPath, "portfolio-v1");
+    const entries = readdirSync(folderPath);
+    entries.forEach((entry) => {
+      const entryPath = join(folderPath, entry);
+      renameSync(entryPath, join(buildPath, entry));
+    });
+    rmdirSync(folderPath);
   },
 } satisfies Config;
